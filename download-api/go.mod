409424c0425e3d3fc0ef54bf96ee0d15aa865eb7
Mikael Kalms <mikael@kalms.org>
2021-12-22 21:51:13 +0100

    Added a hello world style download API service
    Has trouble functioning with VSCode's Cloud Run local emulator

---
+++ download-api/go.mod
@@ -0,0 +1,8 @@
+module download-api
+
+go 1.13
+
+require (
+	cloud.google.com/go v0.99.0
+	github.com/hashicorp/go-retryablehttp v0.7.0
+)
---
+++ download-api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Initialize template parameters.
+	service := os.Getenv("K_SERVICE")
+	if service == "" {
+		service = "???"
+	}
+
+	revision := os.Getenv("K_REVISION")
+	if revision == "" {
+		revision = "???"
+	}
+
+	// Define HTTP server.
+	http.HandleFunc("/", helloRunHandler)
+
+	// PORT environment variable is provided by Cloud Run.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Print("Hello from Cloud Run! The container started successfully and is listening for HTTP requests on $PORT")
+	log.Printf("Listening on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// helloRunHandler responds to requests by rendering an HTML page.
+func helloRunHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("hello run %d", 12)
+	fmt.Fprintf(w, "hello run %d", 12)
+}
