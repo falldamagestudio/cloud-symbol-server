78aea6b28daa142be8b2c321d9e6b8251c4e41f5
Mikael Kalms <mikael@kalms.org>
2021-12-29 10:16:58 +0100

    Storage bucket is no longer publicly accessible

--- infrastructure/core/symbol_store/main.tf
+++ infrastructure/core/symbol_store/main.tf
@@ -6,13 +6,3 @@ resource "google_storage_bucket" "store" {
   uniform_bucket_level_access = true
 }
 
-# Allow anyone to read from symbol store
-resource "google_storage_bucket_iam_member" "store_read_access" {
-
-  depends_on = [ google_storage_bucket.store ]
-
-  bucket   = var.bucket_name
-  role     = "roles/storage.objectViewer"
-  member = "allUsers"
-}
-
