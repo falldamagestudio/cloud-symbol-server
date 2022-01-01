154a81c70424cdcdadc2b8da65e7ae0c8fb6166c
Mikael Kalms <mikael@kalms.org>
2022-01-02 00:00:33 +0100

    Removed hover from PATs

--- firebase/frontend/src/components/PAT.vue
+++ firebase/frontend/src/components/PAT.vue
@@ -1,132 +1,130 @@
 <template>
-  <v-hover v-slot:default="{ hover }">
-    <v-card
-      :elevation="hover ? 6 : 2"
-    >
+  <v-card
+    :elevation="2"
+  >
 
-      <v-card-text>
-        <v-row>
+    <v-card-text>
+      <v-row>
 
-          <!-- Personal Access Token ID -->
+        <!-- Personal Access Token ID -->
 
-          <v-col>
-            {{ pat.id }}
-          </v-col>
+        <v-col>
+          {{ pat.id }}
+        </v-col>
 
-          <v-spacer/>
+        <v-spacer/>
 
-          <v-col
-            class="text-right"
+        <v-col
+          class="text-right"
+        >
+
+          <!-- "Use" modal dialog box -->
+
+          <v-dialog
+            v-model="useDialogVisible"
+            width="500"
           >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+              >
+                Use
+              </v-btn>
+            </template>
 
-            <!-- "Use" modal dialog box -->
+            <v-card>
+              <v-card-title>
+                How to use this PAT
+              </v-card-title>
 
-            <v-dialog
-              v-model="useDialogVisible"
-              width="500"
-            >
-              <template v-slot:activator="{ on, attrs }">
+              <v-card-text>
+                <p>To configure Visual Studio to access the symbol store using this PAT, do the following:
+                </p>
+
+                <ol>
+                  <li>
+                    Run the following command to open the Environment Variables editor:
+                    <code>{{openEnvironmentVariablesCommand}}&nbsp;
+                      <v-btn
+                        icon
+                        @click="copyTextToClipboard(openEnvironmentVariablesCommand)"
+                      >
+                        <v-icon
+                          small
+                        >
+                          mdi-content-copy
+                        </v-icon>
+                      </v-btn>
+                    </code>
+                  </li>
+                  <li>
+                    <p>Create a new Environment Variable for the current user with the following name:
+                    <code>{{ntSymbolPathName}}&nbsp;
+                      <v-btn
+                        icon
+                        @click="copyTextToClipboard(ntSymbolPathName)"
+                      >
+                        <v-icon
+                          small
+                        >
+                          mdi-content-copy
+                        </v-icon>
+                      </v-btn>
+                    </code>
+                    </p>
+                    <p>If this variable already exists, be careful to merge the changes below with the previous settings.
+                    </p>
+                  </li>
+                  <li>
+                    Set the value of the variable to the following:
+                    <code>{{ntSymbolPathValue}}&nbsp;
+                      <v-btn
+                        icon
+                        @click="copyTextToClipboard(ntSymbolPathValue)"
+                      >
+                        <v-icon
+                          small
+                        >
+                          mdi-content-copy
+                        </v-icon>
+                      </v-btn>
+                    </code>
+                  </li>
+                  <li>
+                    If the local symbols folder mentioned in the previous doesn't exist on your machine, create it.
+                  </li>
+                </ol>
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
                 <v-btn
-                  v-bind="attrs"
-                  v-on="on"
+                  color="primary"
+                  @click="useDialogVisible = false"
                 >
-                  Use
+                  OK
                 </v-btn>
-              </template>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
 
-              <v-card>
-                <v-card-title>
-                  How to use this PAT
-                </v-card-title>
+          <!-- "Revoke" button -->
 
-                <v-card-text>
-                  <p>To configure Visual Studio to access the symbol store using this PAT, do the following:
-                  </p>
+          <v-btn
+            color="error--text"
+            v-on:click="revoke()"
+          >
+            Revoke
+          </v-btn>
+        </v-col>
 
-                  <ol>
-                    <li>
-                      Run the following command to open the Environment Variables editor:
-                      <code>{{openEnvironmentVariablesCommand}}&nbsp;
-                        <v-btn
-                          icon
-                          @click="copyTextToClipboard(openEnvironmentVariablesCommand)"
-                        >
-                          <v-icon
-                            small
-                          >
-                            mdi-content-copy
-                          </v-icon>
-                        </v-btn>
-                      </code>
-                    </li>
-                    <li>
-                      <p>Create a new Environment Variable for the current user with the following name:
-                      <code>{{ntSymbolPathName}}&nbsp;
-                        <v-btn
-                          icon
-                          @click="copyTextToClipboard(ntSymbolPathName)"
-                        >
-                          <v-icon
-                            small
-                          >
-                            mdi-content-copy
-                          </v-icon>
-                        </v-btn>
-                      </code>
-                      </p>
-                      <p>If this variable already exists, be careful to merge the changes below with the previous settings.
-                      </p>
-                    </li>
-                    <li>
-                      Set the value of the variable to the following:
-                      <code>{{ntSymbolPathValue}}&nbsp;
-                        <v-btn
-                          icon
-                          @click="copyTextToClipboard(ntSymbolPathValue)"
-                        >
-                          <v-icon
-                            small
-                          >
-                            mdi-content-copy
-                          </v-icon>
-                        </v-btn>
-                      </code>
-                    </li>
-                    <li>
-                      If the local symbols folder mentioned in the previous doesn't exist on your machine, create it.
-                    </li>
-                  </ol>
-                </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="primary"
-                    @click="useDialogVisible = false"
-                  >
-                    OK
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-
-            <!-- "Revoke" button -->
-
-            <v-btn
-              color="error--text"
-              v-on:click="revoke()"
-            >
-              Revoke
-            </v-btn>
-          </v-col>
-
-        </v-row>
-      </v-card-text>
-      
-    </v-card>
-  </v-hover>
+      </v-row>
+    </v-card-text>
+    
+  </v-card>
 </template>
 
 <script lang="ts">
