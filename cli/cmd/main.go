0a45b734219f8fea80e719ab57b0db74ec5d2a81
Mikael Kalms <mikael@kalms.org>
2022-01-05 09:14:22 +0100

    Create transaction from cmdline params

--- cli/cmd/main.go
+++ cli/cmd/main.go
@@ -1,39 +1,80 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/falldamagestudio/cloud-symbol-store/cli"
 )
 
-func upload() error {
+type TransactionRequest struct {
+	Description string              `json:"description"`
+	Files       []UploadFileRequest `json:"files"`
+}
 
-	hash, err := cli.GetPdbHash("example.pdb")
+type UploadFileRequest struct {
+	FileName string `json:"filename"`
+	Hash     string `json:"hash"`
+}
 
-	if err != nil {
-		log.Printf("Error while parsing PDB: %v\n", err)
-		return err
+func createTransaction(description string, fileNames []string) (*TransactionRequest, error) {
+
+	files := []UploadFileRequest{}
+
+	for _, fileName := range fileNames {
+
+		hash, err := cli.GetPdbHash(fileName)
+
+		if err != nil {
+			log.Printf("Error while parsing PDB: %v\n", err)
+			return nil, err
+		}
+
+		uploadFileRequest := UploadFileRequest{
+			FileName: fileName,
+			Hash:     *hash,
+		}
+
+		files = append(files, uploadFileRequest)
 	}
 
-	log.Printf("Hash: %v\n", hash)
+	transaction := &TransactionRequest{
+		Description: description,
+		Files:       files,
+	}
+
+	return transaction, nil
+}
+
+func uploadTransaction(transactionRequest TransactionRequest) error {
+
+	log.Printf("fake transaction: %v", transactionRequest)
 
 	return nil
 }
 
+func upload(description string, fileNames []string) error {
+	transaction, err := createTransaction(description, fileNames)
+	if err != nil {
+		return err
+	}
+
+	return uploadTransaction(*transaction)
+}
+
 func main() {
 
-	// var verbose bool
-	// flag.BoolVar(&verbose, "verbose", false, "verbose output")
-	// flag.Parse()
-	// if verbose {
-	// 	fmt.Println("verbose is on")
-	// }
+	var verbose bool
+	flag.BoolVar(&verbose, "verbose", false, "verbose output")
+	flag.Parse()
+	if verbose {
+		fmt.Println("verbose is on")
+	}
 
-	// operation := flag.Arg(0)
+	operation := flag.Arg(0)
 
-	// if operation == "upload" {
-	// 	_ = upload(flag.Arg(1))
-	// }
-
-	upload()
+	if operation == "upload" {
+		_ = upload(flag.Arg(1), []string{flag.Arg(2)})
+	}
 }
