9ad6f40d8fd53294ea4e4c47c5eff647fe3ba640
Mikael Kalms <mikael@kalms.org>
2022-01-08 14:26:40 +0100

    Use backend API in csharp client

--- csharp-cli/ClientAPI/Class1.cs
+++ csharp-cli/ClientAPI/Class1.cs
@@ -4,6 +4,11 @@ namespace ClassLib
 {
     public class Class1
     {
-
+        public static void Upload() {
+            Org.OpenAPITools.Api.DefaultApi api = new Org.OpenAPITools.Api.DefaultApi("http://testuser:testpat@localhost:8084");
+            Org.OpenAPITools.Model.UploadTransactionRequest uploadTransactionRequest = new Org.OpenAPITools.Model.UploadTransactionRequest();
+            Org.OpenAPITools.Model.UploadTransactionResponse uploadTransactionResponse = api.CreateTransaction(uploadTransactionRequest);
+            Console.WriteLine(uploadTransactionResponse);
+        }
     }
 }
