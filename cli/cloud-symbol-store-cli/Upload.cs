75133fa33f4fd4b579381d9e617b9786cfea13c5
Mikael Kalms <mikael@kalms.org>
2022-01-09 22:33:18 +0100

    Print leading message when starting upload operation

--- cli/cloud-symbol-store-cli/Upload.cs
+++ cli/cloud-symbol-store-cli/Upload.cs
@@ -16,9 +16,17 @@ namespace CLI
 
         public static int DoUpload(UploadOptions options)
         {
-            var Files = FindMatchingFiles(options.Patterns!);
+            IEnumerable<string> files = FindMatchingFiles(options.Patterns!);
 
-            ClientAPI.Ops.Upload(options.ServiceURL, options.Email, options.PAT, options.Description, options.BuildId, Files);
+            Console.WriteLine("Uploading to Cloud Symbol Store...");
+            Console.WriteLine($"  Description: {options.Description}");
+            Console.WriteLine($"  Build ID: {options.BuildId}");
+            Console.WriteLine("  Files:");
+            foreach (string file in files) {
+                Console.WriteLine($"    {file}");
+            }
+
+            ClientAPI.Ops.Upload(options.ServiceURL, options.Email, options.PAT, options.Description, options.BuildId, files);
 
             return 0;
         }
