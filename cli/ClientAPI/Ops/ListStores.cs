56e101e2b2c53fa39446be14e7f0682592d5e370
Mikael Kalms <mikael@kalms.org>
2022-01-18 08:58:51 +0100

    Removed unnecessary using's

--- cli/ClientAPI/Ops/ListStores.cs
+++ cli/ClientAPI/Ops/ListStores.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using BackendAPI.Model;
-using RestSharp;
 
 namespace ClientAPI
 {
--- cli/ClientAPI/Ops/Upload.cs
+++ cli/ClientAPI/Ops/Upload.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using BackendAPI.Model;
 using RestSharp;
 
 namespace ClientAPI
