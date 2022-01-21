849bd5c501ceddd729296d69dc6b3827e7b00f1c
Mikael Kalms <mikael@kalms.org>
2022-01-21 16:55:32 +0100

    Simplified usage guide, based around  Visual Studio
    This is how most developers would use symbol stores today

--- firebase/frontend/src/components/PATUsageGuide.vue
+++ firebase/frontend/src/components/PATUsageGuide.vue
@@ -10,7 +10,7 @@
     >
       <v-tabs-slider></v-tabs-slider>
       <v-tab>
-        Debugger
+        Visual Studio
       </v-tab>
       <v-tab>
         Windows CLI
@@ -24,78 +24,21 @@
       <v-tab-item>
         <v-card flat>
           <v-card-text>
-            <p>To configure Visual Studio to access the symbol server using this PAT, do the following:
+            <p>To configure Visual Studio to access the symbol server using this PAT, visit <code>Tools | Options... | Debugging | Symbols</code>, and add the following symbol file location:
+              <code>{{symbolServerDownloadAPIEndpoint}}&nbsp;
+                <v-btn
+                  icon
+                  @click="copyTextToClipboard(symbolServerDownloadAPIEndpoint)"
+                >
+                  <v-icon
+                    small
+                  >
+                    mdi-content-copy
+                  </v-icon>
+                </v-btn>
+              </code>
             </p>
-
-            <ol>
-              <li>
-                Decide on a location to cache symbol files on your machine, for example
-                <code>{{symbolCacheLocation}}&nbsp;
-                  <v-btn
-                    icon
-                    @click="copyTextToClipboard(symbolCacheLocation)"
-                  >
-                    <v-icon
-                      small
-                    >
-                      mdi-content-copy
-                    </v-icon>
-                  </v-btn>
-                </code>
-                . Create the folder if it does not exist.
-              </li>
-              <li>
-                Run the following command to open the Environment Variables editor:
-                <code>{{openEnvironmentVariablesCommand}}&nbsp;
-                  <v-btn
-                    icon
-                    @click="copyTextToClipboard(openEnvironmentVariablesCommand)"
-                  >
-                    <v-icon
-                      small
-                    >
-                      mdi-content-copy
-                    </v-icon>
-                  </v-btn>
-                </code>
-              </li>
-              <li>
-                Create or edit an environment variable for the current user with the following name:
-                <code>{{ntSymbolPathName}}&nbsp;
-                  <v-btn
-                    icon
-                    @click="copyTextToClipboard(ntSymbolPathName)"
-                  >
-                    <v-icon
-                      small
-                    >
-                      mdi-content-copy
-                    </v-icon>
-                  </v-btn>
-                </code>
-              </li>
-              <li>
-                <p>Set the value of the variable to the following:
-                <code>{{ntSymbolPathValue}}&nbsp;
-                  <v-btn
-                    icon
-                    @click="copyTextToClipboard(ntSymbolPathValue)"
-                  >
-                    <v-icon
-                      small
-                    >
-                      mdi-content-copy
-                    </v-icon>
-                  </v-btn>
-                </code>
-                </p>
-                <p>If this variable already exists, be careful to merge the changes above with its previous settings.
-                </p>
-                <p>Adjust the symbol cache folder location if necessary.
-                </p>
-              </li>
-            </ol>
-            <p>For more information on _NT_SYMBOL_PATH, see <a href="https://docs.microsoft.com/en-us/windows/win32/debug/using-symsrv#setting-the-symbol-path">the MSDN documentation</a>.
+            <p>If you rather wish to use <code>_NT_SYMBOL_PATH</code> directly, see <a href="https://docs.microsoft.com/en-us/windows/win32/debug/using-symsrv#setting-the-symbol-path">the MSDN documentation</a>.
             </p>
           </v-card-text>
         </v-card>
@@ -138,10 +81,7 @@ import { adminAPIEndpoint, downloadAPIEndpoint } from '../firebaseConfig'
 interface Data {
   howToUseTab: any,
   downloadConfigFileHref: string,
-  symbolCacheLocation: string,
-  openEnvironmentVariablesCommand: string,
-  ntSymbolPathName: string,
-  ntSymbolPathValue: string,
+  symbolServerDownloadAPIEndpoint: string,
 }
 
 export default Vue.extend({
@@ -159,10 +99,7 @@ export default Vue.extend({
         'email': this.email,
         'pat': this.pat.id,
       }, null, 2)),
-      symbolCacheLocation: 'C:\\Symbols',
-      openEnvironmentVariablesCommand: 'rundll32 sysdm.cpl,EditEnvironmentVariables',
-      ntSymbolPathName: '_NT_SYMBOL_PATH',
-      ntSymbolPathValue: 'SRV*C:\\Symbols*' + downloadAPIEndpoint.split('://')[0] + "://" + encodeURIComponent(this.email) + ':' + this.pat.id + '@' + downloadAPIEndpoint.split('://')[1],
+      symbolServerDownloadAPIEndpoint: downloadAPIEndpoint.split('://')[0] + "://" + encodeURIComponent(this.email) + ':' + this.pat.id + '@' + downloadAPIEndpoint.split('://')[1],
     }
   },
 
