db45c7eb568dd4821aa706e503529a2156074b96
Mikael Kalms <mikael@kalms.org>
2022-11-21 15:51:50 +0100

    Better table design

--- db-migrations/000001_files.down.sql
+++ db-migrations/000001_files.down.sql
@@ -1,1 +1,5 @@
+DROP TABLE cloud_symbol_server.stores;
+DROP TYPE cloud_symbol_server.upload_status;
+DROP TABLE cloud_symbol_server.uploads;
+DROP TYPE cloud_symbol_server.file_status;
 DROP TABLE cloud_symbol_server.files;
--- db-migrations/000001_files.up.sql
+++ db-migrations/000001_files.up.sql
@@ -1,6 +1,39 @@
+
+CREATE TABLE cloud_symbol_server.stores (
+  store_id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name varchar NOT NULL UNIQUE
+);
+
+CREATE TYPE cloud_symbol_server.upload_status AS ENUM (
+  'unknown',
+  'in_progress',
+  'completed',
+  'aborted',
+  'expired'
+);
+
+CREATE TABLE cloud_symbol_server.uploads (
+  upload_id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  store_id integer REFERENCES cloud_symbol_server.stores,
+  description varchar NOT NULL,
+  build varchar NOT NULL,
+  timestamp timestamp NOT NULL,
+  status cloud_symbol_server.upload_status NOT NULL
+);
+
+CREATE TYPE cloud_symbol_server.file_status AS ENUM (
+  'unknown',
+  'already_present',
+  'pending',
+  'uploaded',
+  'aborted',
+  'expired'
+);
+
 CREATE TABLE cloud_symbol_server.files (
-  id serial PRIMARY KEY,
+  file_id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  upload_id integer REFERENCES cloud_symbol_server.uploads,
   file_name varchar NOT NULL,
   hash varchar NOT NULL,
-  status varchar NOT NULL
+  status cloud_symbol_server.file_status NOT NULL
 );
