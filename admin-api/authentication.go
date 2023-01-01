d5591f38c2b159cd06bfa5982155fdf4812aa901
Mikael Kalms <mikael@kalms.org>
2023-01-01 22:29:46 +0100

    Restructured PAT auth logic

--- admin-api/authentication.go
+++ admin-api/authentication.go
@@ -25,45 +25,63 @@ func writePatHttpError(w http.ResponseWriter, status int, message string) error 
 	return nil
 }
 
+func validateUsernamePassword(r *http.Request) (bool, int) {
+
+	// Fetch email + PAT from Basic Authentication header of WWW request
+
+	email, pat, basicAuthPresent := r.BasicAuth()
+
+	if !basicAuthPresent {
+
+		log.Print("Basic auth header (with email/token) not provided")
+		return false, 0
+	}
+
+	log.Printf("Basic Auth header present, email: %v, PAT: %v", email, pat)
+
+	// Validate that email + PAT exists in database
+
+	firestoreClient, err := helpers.FirestoreClient(r.Context())
+	if err != nil {
+		log.Printf("Unable to talk to database: %v", err)
+		return false, http.StatusInternalServerError
+	}
+
+	patDocRef := firestoreClient.Collection("users").Doc(email).Collection("pats").Doc(pat)
+
+	if _, err := patDocRef.Get(r.Context()); err != nil {
+
+		log.Printf("Unable to find email %v, pat %v combination in database: %v", email, pat, err)
+		return false, http.StatusUnauthorized
+	}
+
+	log.Printf("Email/PAT pair %v, %v exist in database; authentication successful", email, pat)
+	return true, 0
+}
+
 func (patAM *patAuthenticationMiddleware) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// Fetch email + PAT from Basic Authentication header of WWW request
+		// Authenticate via username + password combination
 
-		email, pat, basicAuthPresent := r.BasicAuth()
+		authenticated, statusCode := validateUsernamePassword(r)
+		if statusCode != 0 {
 
-		if !basicAuthPresent {
+			if statusCode == http.StatusUnauthorized {
+				w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+				_ = writePatHttpError(w, http.StatusUnauthorized, "Unauthorized; please provide valid email + personal access token when using Basic Authentication")
+				return
+			} else {
+				_ = writePatHttpError(w, http.StatusInternalServerError, "Internal server error")
+				return
+			}
+		}
 
-			log.Print("Basic auth header (with email/token) not provided")
-
+		if !authenticated {
 			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-			_ = writePatHttpError(w, http.StatusUnauthorized, "Unauthorized; please provide email + personal access token using Basic Authentication")
+			_ = writePatHttpError(w, http.StatusUnauthorized, "Unauthorized; please provide valid email + personal access token")
 			return
 		}
-
-		log.Printf("Basic Auth header present, email: %v, PAT: %v", email, pat)
-
-		// Validate that email + PAT exists in database
-
-		firestoreClient, err := helpers.FirestoreClient(r.Context())
-		if err != nil {
-			log.Printf("Unable to talk to database: %v", err)
-			_ = writePatHttpError(w, http.StatusInternalServerError, "Unable to talk to database")
-			return
-		}
-
-		patDocRef := firestoreClient.Collection("users").Doc(email).Collection("pats").Doc(pat)
-
-		if _, err := patDocRef.Get(r.Context()); err != nil {
-
-			log.Printf("Unable to find email %v, pat %v combination in database: %v", email, pat, err)
-
-			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-			_ = writePatHttpError(w, http.StatusUnauthorized, "Unauthorized; unable to find email / pat combination in database")
-			return
-		}
-
-		log.Printf("Email/PAT pair %v, %v exist in database; authentication successful", email, pat)
 
 		next.ServeHTTP(w, r)
 	})
