23e8a6319fecfbdbcb1fe274ddc7fbe57e3a5ec2
Mikael Kalms <mikael@kalms.org>
2023-01-04 14:30:58 +0100

    Removed auth token refresh-on-error logic
    The frontend will ensure that the auth token is
    fresh before sending each request; no need for
    having auth-error-refresh-retry logic

--- firebase/frontend/src/adminApi.ts
+++ firebase/frontend/src/adminApi.ts
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import createAuthRefreshInterceptor from 'axios-auth-refresh'
-import { getIdToken } from 'firebase/auth'
 
 import { Configuration, ConfigurationParameters } from './generated/configuration'
 import { DefaultApiFactory } from './generated/api'
@@ -12,39 +10,16 @@ const axiosInstance = axios.create({
   timeout: 5000,
 });
 
-// Obtain the fresh token each time the function is called
-function getAccessToken() {
-  return localStorage.getItem('token');
-}
-
 // Use interceptor to inject the token to requests
 axiosInstance.interceptors.request.use((request: any) => {
   const authUserStore = useAuthUserStore()
-  console.log("request interceptor called")
 
   return authUserStore.user!.getIdToken(false)
     .then((token) => {
-      console.log("getIdToken callback resulted in token: " + token)
       request.headers['Authorization'] = 'Bearer ' + token;
       return request;
     })
 });
-
-// Function that will be called to refresh authorization
-// const refreshAuthLogic = (failedRequest: any) => {
-//   const authUserStore = useAuthUserStore()
-//   console.log("refreshAuthLogic called")
-
-//   return authUserStore.user!.getIdToken(false)
-//     .then((token) => {
-//       console.log("getIdToken callback resulted in token: " + token)
-//       failedRequest.response.config.headers['Authorization'] = 'Bearer ' + token;
-//       return Promise.resolve();
-//     })
-// }
-
-// Instantiate the interceptor
-// createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic);
 
 const apiConfigurationParameters = {
   basePath: adminAPIEndpoint
